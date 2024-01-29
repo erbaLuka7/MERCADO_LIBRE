185b785a9cb0611e387e470547444ddb482402e8
erbaLuka7 <ezequielerba7@gmail.com>
2024-01-29 17:02:12 -0300

    login terminado

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ app.get("/",(req,res)=>{
 app.get("/register", (req,res)=>{
     res.sendFile(path.join(__dirname,"/views/register.html"))
 })
+app.get("/login", (req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/login.html"))
+})
 
 
 app.listen(port,()=> console.log(`http://localhost:${port}`))
